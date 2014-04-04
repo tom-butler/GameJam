c1fbdab4aefc6311aa76e0fcd4cabd574667fab5
Tom Butler <tombutler.development@gmail.com>
2014-04-05 10:51:27 +1100

    Inintial Commit

---
+++ GameJam1/GameJam1/Game1.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using GameLibrary;
+
+namespace GameJam1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        //global
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        string state;
+        SpriteList sprites;
+        Texture2D background;
+
+        //constants
+        const int WINDOW_HEIGHT = 640;
+        const int WINDOW_WIDTH = 800;
+        static Vector2 WINDOW_CENTRE = new Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            //initilise number of sprites
+            sprites = new SpriteList(100);
+
+            //set starting state should be menu (later)
+            state = "playing";
+
+            //set the window size
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            //load the background texture
+            background = this.Content.Load<Texture2D>(@"images/sand");
+            //load the player
+            //load textures here
+
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.SandyBrown);
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearWrap, null, null);
+
+            #region draw
+            //draw the background
+            spriteBatch.Draw(background, new Vector2(0, 0), new Rectangle((int) WINDOW_WIDTH, (int) WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT), Color.White);
+
+            #endregion
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GameLibrary/GameLibrary/Level.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+//using Microsoft.Xna.Framework.Net;
+//using Microsoft.Xna.Framework.Storage;
+
+
+namespace GameLibrary
+{
+    /// <summary>
+    /// Parent level class all levels inherit from this
+    /// </summary>
+    public abstract class UC_LevelParent
+    {
+        public GraphicsDevice gd;
+        public SpriteBatch spriteBatch;
+        
+        public ContentManager Content;
+        public UC_LevelManager levelManager;
+
+        public KeyboardState keyState; // for convenience - not really needed
+        public KeyboardState prevKeyState; // for convenience not really needed
+
+        public float mouse_x = 0;
+        public float mouse_y = 0; // for convenience not really needed
+        public MouseState currentMouseState; // for convenience not really needed
+        public MouseState previousMouseState; // for convenience not really needed
+
+        public SpriteFont font1; // use if you want again not really needed
+
+        public virtual void InitializeLevel(GraphicsDevice g, SpriteBatch s, ContentManager c, UC_LevelManager lm)
+        {
+            gd = g;
+            spriteBatch = s;
+            Content = c;
+            levelManager = lm;
+        }
+
+        public virtual void LoadContent() { }
+        public virtual void UnloadContent() { }
+        public virtual void EnterLevel(int fromLevelNum) { } // runs on set and push
+        public virtual void ExitLevel() { } // runs on set and pop
+        public virtual void Update(GameTime gameTime) { }
+        public abstract void Draw(GameTime gameTime);
+
+        /// <summary>
+        /// Utility routine to set up keyboard and mouse
+        /// </summary>
+        public void getKeyboardAndMouse()
+        {
+
+            prevKeyState = keyState;
+            keyState = Keyboard.GetState();
+
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+
+            mouse_x = currentMouseState.X;
+            mouse_y = currentMouseState.Y;
+        }
+
+    }
+
+    /// <summary>
+    /// To manage levels  
+    /// </summary>
+    public class UC_LevelManager
+    {
+        UC_LevelParent[] levels;
+        UC_LevelParent cur; //current_Level;        
+        UC_LevelParent prevLevel; //previous level;
+        int curLevNum;
+
+        int[] levelStack;
+        int sp; // stack pointer
+
+        public UC_LevelManager()
+        {
+            init(100);
+
+        }
+
+        private void init(int numLevelz)
+        {
+            levels = new UC_LevelParent[numLevelz];
+            for (int i = 0; i < numLevelz; i++) levels[i] = null;
+            levelStack = new int[30];
+            sp = 0;
+            setEmptyLevel();
+        }
+
+        public void AddLevel(int levNum, UC_LevelParent lev)
+        {
+            levels[levNum] = lev;
+        }
+
+        public UC_LevelParent getLevel(int levNum)
+        {
+            return levels[levNum];
+        }
+
+        public void setLevel(int levNum)
+        {
+            prevLevel = cur;
+            levels[levNum].EnterLevel(curLevNum);
+            cur = levels[levNum];
+            prevLevel.ExitLevel();
+            curLevNum = levNum;
+
+            cur.prevKeyState = Keyboard.GetState();
+            cur.keyState = Keyboard.GetState(); // fix legacy keystate issues
+            cur.previousMouseState = Mouse.GetState();
+            cur.currentMouseState = Mouse.GetState();
+        }
+
+        public void pushLevel(int levNum)
+        {
+            prevLevel = cur;
+            levels[levNum].EnterLevel(curLevNum);
+            levelStack[sp] = curLevNum;
+            cur = levels[levNum];
+            curLevNum = levNum;
+            sp++;
+        }
+
+        public int popLevel()
+        {
+            sp--;
+            prevLevel = cur;
+            cur = levels[levelStack[sp]];
+            curLevNum = levelStack[sp];
+            prevLevel.ExitLevel();
+
+            cur.prevKeyState = Keyboard.GetState();
+            cur.keyState = Keyboard.GetState(); // fix legacy keystate issues
+            cur.previousMouseState = Mouse.GetState();
+            cur.currentMouseState = Mouse.GetState();
+            return curLevNum;
+        }
+
+        public void setEmptyLevel()
+        {
+            cur = new EmptyLevel();
+            curLevNum = -1; // hmm i guess empty level is now level -1
+        }
+
+        public UC_LevelParent getCurrentLevel()
+        {
+            return cur;
+        }
+
+        public int getCurrentLevelNum()
+        {
+            return curLevNum;
+        }
+    }
+
+    /// <summary>
+    /// A default 'empty' level to fix probelms of having nothing to Draw or Update
+    /// </summary>
+    class EmptyLevel : UC_LevelParent
+    {
+        public override void Draw(GameTime gameTime)
+        {
+        }
+    }
+}
